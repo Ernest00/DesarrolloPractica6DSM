a199eee707521d1413008d651702ff4facaf7ff6
Ernest00 <ernesto.anaya2014@gmail.com>
2023-03-05 17:46:29 -0600

    Add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Desarrollo de Práctica 6 - DSM104
+
+Desarrollo de Software para Móviles DSM104.
+
+* **Marroquín Anaya, Bryan Ernesto - MA181956**
---
+++ app/src/main/java/com/example/myapplicationpractica06_dsm104/FrameLayout.kt
@@ -0,0 +1,15 @@
+package com.example.myapplicationpractica06_dsm104
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class FrameLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+    fun finalizarActividad(v: View?){
+        finish()
+    }
+}
